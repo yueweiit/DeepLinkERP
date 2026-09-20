212d41f88524f68b0a3fe86b0a4548e29679d351
w j z <zwj07253@gmail.com>
2026-09-20 08:05:18 +0800

    补齐ERPNext依赖与持续集成配置

--- mes_integration/hooks.py
+++ mes_integration/hooks.py
@@ -8,7 +8,7 @@ app_license = "mit"
 # Apps
 # ------------------
 
-# required_apps = []
+required_apps = ["erpnext"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
